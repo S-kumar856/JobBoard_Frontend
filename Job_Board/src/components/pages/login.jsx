baa6c5f2b7e85b3dac201c632a0d10de96725aeb
S KUMAR <hk856856@gmail.com>
2024-12-10 23:34:16 +0530

    third

--- Job_Board/src/components/pages/login.jsx
+++ Job_Board/src/components/pages/login.jsx
@@ -1,44 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { loginService } from "../../services";
 import { useNavigate } from "react-router-dom";
 
-function Login(){
-    // i user already logged in then automatically it open the home page
-    if (localStorage.getItem('token')){
-        alert('Already logged in');
-        navigate('/home');
-    }
+function Login() {
     const navigate = useNavigate();
+
     const [loginFormData, setLoginFormData] = useState({
         email: "",
         password: ""
     })
 
-    const handleLogin = (e) =>{
-        const {name, value} = e.target;
-        setLoginFormData({...loginFormData, [name]:value})
+    const handleLogin = (e) => {
+        const { name, value } = e.target;
+        setLoginFormData({ ...loginFormData, [name]: value })
     }
 
-    const handleLoginSubmit = async(e) =>{
+    const handleLoginSubmit = async (e) => {
         e.preventDefault();
         const res = await loginService(loginFormData);
-        if (res.status === 200){
+        if (res.status === 200) {
             localStorage.setItem('token', res.token)
             alert("Login successfull")
             navigate("/home");
         }
-        else{
+        else {
             alert("Invalid credentials")
         }
-        setLoginFormData("");   
-      
+        setLoginFormData("");
     }
 
-    return(
+    // if user already logged in, then automatically it open the home page
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+            alert('Already logged in');
+            navigate('/home');
+        }
+    },[])
+
+    return (
         <>
             <form onSubmit={handleLoginSubmit}>
                 <input type="email" value={loginFormData.email} onChange={handleLogin} name="email" id="email" placeholder="Enter your email" />
-                <input type="password" value={loginFormData.password}  onChange={handleLogin} name="password" id="password" placeholder="Enter your password" />
+                <input type="password" value={loginFormData.password} onChange={handleLogin} name="password" id="password" placeholder="Enter your password" />
                 <button type="submit">Login</button>
             </form>
 
